ecf702754d7bca0c5fc1d46f07a7d55eb5e2edad
Sagar Shinde <asagarshinde@gmail.com>
2017-08-26 21:55:29 +0530

    Add files via upload
    In this version addition of days is added with time.

--- time_arith.py
+++ time_arith.py
@@ -5,16 +5,18 @@ This module takes start and end time wth space separated hour, minute and second
 e.g. 10 20 30 i.e  10 hours, 20 minutes and 30 seconds
 
 example:
-import time_arith as t
+from time_arith import time_arg, add_time
 start = [10,10,10]
 end = [10,10,30]
-start_time = t.time_arg(start)  # it will add time to Time class
-end_time = t.time_arg(end)
+start_time = time_arg(start)  # it will add time to Time class
+end_time = time_arg(end)
 
-addition = t.add_time(start_time,end_time)
+addition = add_time(start_time,end_time)
 print (addition.hour)
 print (addition.second)
 print (addition.minute)
+
+This module will not support output in form of days
 """
 
 class Time():
@@ -23,7 +25,8 @@ class Time():
      """
 
 def time_to_int(time):
-     minutes = time.hour*60 + time.minute
+     hours = time.day*24 + time.hour
+     minutes = hours*60 + time.minute
      seconds = minutes*60 + time.second
      return seconds
 
@@ -31,6 +34,7 @@ def int_to_time(seconds):
      time = Time()
      minutes, time.second = divmod(seconds, 60)
      time.hour, time.minute = divmod(minutes, 60)
+     time.day, time.hour = divmod(time.hour,24)
      return time
 
 def add_time(start, end):
@@ -47,10 +51,15 @@ def sub_time(start, end):
           print ("Start time is greator than end time.")
      
 def time_arg(time_string):
+     """
+     This function will create object of Time class
+
+     """
      time = Time()
-     time.hour = int(time_string[0])
-     time.minute = int(time_string[1])
-     time.second = int(time_string[2])
+     time.day = int(time_string[0])
+     time.hour = int(time_string[1])
+     time.minute = int(time_string[2])
+     time.second = int(time_string[3])
      return time
 
 def main():
@@ -70,5 +79,4 @@ def time_mod(*args):
 
 if __name__ == '__main__':
      main()
-#else:
-#     time_mod()
+
